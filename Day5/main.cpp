8e5e4235dbeba64e1f7052f380ef49e4aa8069a5
bahadirsulukan <bahadirsulukan@gmail.com>
2025-10-18 23:53:48 +0200

    Basic Division Exercise

---
+++ Day5/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int result;
+    int number1 = 16;
+    int number2 = 8;
+    result = number1 / number2;
+    cout << result << endl;
+    return 0;
+}
+// This program performs integer division of two numbers and prints the result.
+// Expected output: 2
