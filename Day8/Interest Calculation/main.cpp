2202be3516deac501547e17e9a8865f02c0a1b8b
bahadirsulukan <bahadirsulukan@gmail.com>
2025-10-19 00:19:23 +0200

    Interest Calculation

---
+++ Day8/Interest Calculation/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double Anfangskapital, Zinssatz;
+    int Laufzeit;
+
+    cout << "Bitte geben Sie das Anfangskapital: " << endl;
+    cin >> Anfangskapital;
+    cout << "Bitte geben Sie den Zinssatz (%): " << endl;
+    cin >> Zinssatz;
+    cout << "Bitte geben Sie die Laufzeit (Jahr): " << endl;
+    cin >> Laufzeit;
+
+    // wir verwenden for Schleife weil wir Variablen benutzen
+
+    double Kapital = Anfangskapital;
+    for (int Jahr = 1; Jahr <= Laufzeit; Jahr++)
+    {
+
+        double Zinsen = Kapital * Zinssatz / 100;
+
+        Kapital += Zinsen;
+
+        cout << "Jahr " << Jahr << ": " << Kapital << endl;
+    }
+}
