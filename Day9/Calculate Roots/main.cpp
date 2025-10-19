1de31fe1fa2e680c5efc9c76f2135fe5ca587578
bahadirsulukan <bahadirsulukan@gmail.com>
2025-10-20 01:36:02 +0200

    The program calculates the square root of a positive number with the desired accuracy

---
+++ Day9/Calculate Roots/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    long double x, a;
+    long double y = 1.0;
+    int genau;
+
+    cout << "Geben Sie ein Zahl ein :";
+    cin >> a;
+    cout << "Geben Sie Genauigkeit Nachkommastellen ein (max.50) \n";
+    cin >> genau;
+    x = a;
+
+    if (x < y)
+    {
+        cout << "Fehler: Bitte geben Sie positive Zahl ein \n";
+    }
+    else
+    {
+        while (x > y)
+        {
+            x = (x + y) / 2;
+            y = a / x;
+        }
+        cout << "Die Würzel betraegt" << setprecision(genau) << fixed << x << "\n";
+    }
+}
