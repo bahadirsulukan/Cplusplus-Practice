846d018e5b54098075aec4298ac4b609c6f9a148
bahadirsulukan <bahadirsulukan@gmail.com>
2025-10-20 01:32:33 +0200

    A program reads a series of positive numbers and returns the minimum value

---
+++ Day9/Mindestwert/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int m;
+
+    cout << "Bitte geben Sie die Anzahl der Zahlen ein : " << endl;
+    cin >> m;
+
+    int Arr[m];
+
+    for (int i = 0; i < m; ++i)
+    {
+        cout << "Bitte geben Sie das " << i + 1 << ". Elemente ein : " << endl;
+        cin >> Arr[i];
+        while (Arr[i] < 0)
+        {
+            cout << "Bitte geben Sie positive Zahlen ein: ";
+            cin >> Arr[i];
+        }
+    }
+
+    int min = Arr[0];
+    for (int i = 1; i < m; ++i)
+    {
+
+        if (Arr[i] < min)
+        {
+            min = Arr[i];
+        }
+    }
+
+    cout << "Mindestwert des Arrays: " << min << endl;
+}
