5b7127fdb07da095c0c2b6e89f734cb04ebd1b07
bahadirsulukan <bahadirsulukan@gmail.com>
2025-11-02 23:42:45 +0100

    // The following class demonstrates the use of 'this' pointer in C++

---
+++ Day21/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+private:
+    int x, y;
+
+public:
+    int getX();
+    void setX(int);
+
+    int getY();
+    void setY(int);
+};
+
+int A::getX()
+{
+    return x;
+}
+int A::getY()
+{
+    return y;
+}
+
+void A::setX(int x)
+{
+    this->x = x; // this Pointeri
+}
+
+void A::setY(int y)
+{
+    if (y > 5)
+        this->y = y;
+    else
+        this->y = 2;
+}
+
+int main()
+{
+}
